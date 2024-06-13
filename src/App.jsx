4e87216b7e93f4d4ae13f09c98042210f5f6974d
Mridani Pandey <mridanipandey8813@gmail.com>
2024-06-13 17:25:34 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react';
+import './App.css'
+import Footer from './footer';
+
+  const  App = () => {
+
+  return (
+    <>
+      <div>
+        <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default App;
